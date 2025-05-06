39acc19df7453e6a9fa072f6491be3dfa609a188
Eren Steliyanov <erenevgeniev@gmail.com>
2025-05-07 01:16:52 +0300

    Removed Unnecessary Buttons
    - Removed Home And Privacy Button
    - Added little logo for tabs

--- GymManager/obj/Debug/net8.0/GymManager.AssemblyInfo.cs
+++ GymManager/obj/Debug/net8.0/GymManager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GymManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9b3b0281d959aaada8be71d29a609eb913a6b99")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf7b3405c735f275bca5edd8603dabef25bb9dc4")]
 [assembly: System.Reflection.AssemblyProductAttribute("GymManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GymManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
